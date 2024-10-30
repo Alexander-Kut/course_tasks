a0a74e412fb0faad5357f8625343223fc15f3e78
Alexander-Kut <a.v.kutikov2024@gmail.com>
2024-10-30 21:58:37 +0400

    createdJS-script

---
+++ hw-2.js
@@ -0,0 +1,40 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+let firstIphoneYear = Number(2007);
+alert = firstIphoneYear;
+
+let nameInventor = "Brendan Eich";
+alert(nameInventor);
+
+let first = 10, second = 2;
+alert(first + second);
+alert(first - second);
+alert(first * second);
+alert(first / second);
+
+let resuslt = 2**5;
+alert(resuslt);
+
+let A = 9, B = 2;
+remainder = A % B;
+alert(remainder);
+
+let num = 1;
+num +=5;
+num -=3;
+num *=7;
+num /=3;
+++num;
+--num;
+alert(num);
+
+age = prompt('How old are you?');
+alert(age);
+;
+user = {name: "Brendan Eich", age: 63, isAdmin: false};
+
+userName = prompt("What's your name?");
+alert(`Hello, ${userName}!`);
