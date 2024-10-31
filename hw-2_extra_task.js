ffb0e2b34985851ee1bfb81fca836699a78dcb44
Alexander-Kut <a.v.kutikov2024@gmail.com>
2024-10-31 18:04:58 +0400

    extraTask

---
+++ hw-2_extra_task.js
@@ -0,0 +1,14 @@
+let number = Number(prompt("Please, enter any number:", "7"));
+let numberDoubled = number * 2;
+alert("Your doubled number is: " + numberDoubled);
+
+let numberAdded = numberDoubled + 10;
+alert(`Your added (+10) number is: ${numberAdded}`);
+
+let numberDivided = numberAdded / 2;
+alert(`Divided number is: ${numberDivided}`);
+
+let numberSubtracted = numberDivided - number;
+alert(`Subtracted number is: ${numberSubtracted}`);
+
+alert ("Your number is 5!");
