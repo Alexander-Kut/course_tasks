34451ee5c9af34b6328d61d88f9749137eba0c0c
Alexander-Kut <a.v.kutikov2024@gmail.com>
2024-11-07 21:34:27 +0400

    JSMainTaskDone

---
+++ hw-4_extra_task.js
@@ -0,0 +1,15 @@
+// //Task#1
+
+// let k = 100;
+// let iterations = 0;
+// while (k > 0) {
+//     console.log(k -=7);
+//     iterations++;
+//     console.log(iterations);
+// }
+
+// Task#2
+
+const months = ["January", 'February', 'March', 'April',
+                 'May', 'June', 'July', 'August', 
+                 'September', 'October', 'November', 'December'];
