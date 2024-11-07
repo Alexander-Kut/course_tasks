ea962c919a56c9be0ce8109d91fbad7f353dd835
Alexander-Kut <a.v.kutikov2024@gmail.com>
2024-11-07 22:54:39 +0400

    extraTaskAlmostDone

--- hw-4_extra_task.js
+++ hw-4_extra_task.js
@@ -1,15 +1,36 @@
-// //Task#1
+//Task#1
 
-// let k = 100;
-// let iterations = 0;
-// while (k > 0) {
-//     console.log(k -=7);
-//     iterations++;
-//     console.log(iterations);
-// }
+let k = 100;
+let iterations = 0;
+while (k > 0) {
+    console.log(k -=7);
+    iterations++;
+    console.log(iterations);
+}
+
 
 // Task#2
 
 const months = ["January", 'February', 'March', 'April',
                  'May', 'June', 'July', 'August', 
                  'September', 'October', 'November', 'December'];
+
+const monthsLength = months.length;
+
+for (i = 0; i < monthsLength; i++) {
+    console.log(`${i + 1} ${months[i]}`);
+}
+
+
+//Task#3
+
+const myBook = {"название": "\"1984\"", "автор": "Джордж Оруэлл", "год издания": "1949",
+                 "жанр": "Антиутопия"};
+    for (let key in myBook) {
+        console.log(`${key}: ${myBook[key]}`);
+            
+    }
+  
+    
+//Task#4
+
