53f3736d2d422aa7bf7bb94aa223aaf4062bee62
Alexander-Kut <a.v.kutikov2024@gmail.com>
2024-11-19 18:14:17 +0400

    fixedAgain

--- HW_5/guess_a_number.js
+++ HW_5/guess_a_number.js
@@ -1,29 +1,30 @@
 
 
 function guessNumber() {
-    
+
     const randomNumber = Math.floor(Math.random() * 100 + 1);
     console.log(randomNumber);
     let userNumber;
-    let counter;
-    
-    
-    while (userNumber != randomNumber) {
+    let counter = 1;
+
+    do {
         userNumber = Number(prompt('Введите любое число от 1 до 100'));
+        console.log(counter);
         counter++;
 
         if (userNumber > randomNumber) {
-           console.log(`Ваше число больше чем загадано. Попробуйте ещё`);
-            
+            alert(`Ваше число больше чем загадано. Попробуйте ещё`);
+
 
         } else if (userNumber < randomNumber) {
-           console.log(`Ваше число меньше чем загадано. Попробуйте ещё`);
-            
-           
+            alert(`Ваше число меньше чем загадано. Попробуйте ещё`);
+
+
         } else {
-            console.log( `Вы угадали. Спасибо за игру!`);
-            
+            alert(`Вы угадали. Спасибо за игру!`);
+            alert(`Загаданное число: ${randomNumber}`);
+
         }
-          
-    }
+    } while (userNumber != randomNumber);
+
 }  
